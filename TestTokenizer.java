b5a1411057090f2cd808e83350a12e3f4c480d85
Anushri488 <anushrimishra20@gmail.com>
2026-03-23 15:06:38 +0000

    Implemented and tested tokenizer for CALC

---
+++ TestTokenizer.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+public class TestTokenizer {
+    public static void main(String[] args) {
+        String code = """
+                i := 1
+                @ 4 =>
+                >> i
+                i := i + 1
+                """;
+        // String code = """
+        // x := 10 + 3 * 2
+        // """;    Test 1
+
+        // String code = """
+        // name := "Sitare"
+        // >> name
+        // """;   Test 2
+
+
+        // String code = """
+        // ? x > 5 =>
+        // >> "ok"
+        // """;    Test 3
+
+
+
+
+        Tokenizer tokenizer = new Tokenizer(code);
+        List<Token> tokens = tokenizer.tokenize();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+}
