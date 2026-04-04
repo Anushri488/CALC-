d1eb3cc4c3cef30a97940be02e2f04f736f63b03
preranarajnag <preranarajnag2809@gmail.com>
2026-04-05 01:59:18 +0530

    Add variable management to Environment class
    Implemented a variable storage system with methods to set, get, and check existence of variables.

--- Evaluator/Environment.java
+++ Evaluator/Environment.java
@@ -1,5 +1,26 @@
 package Evaluator;
 
-public class Environment {
-  
+import java.util.HashMap;
+import java.util.Map;
+
+public class Environment{
+
+    private final Map<String, Object> variables = new HashMap<>();
+
+    public void set(String name, Object value){
+        variables.put(name, value);
+    }
+
+    public Object get(String name){
+        if (!variables.containsKey(name)){
+            throw new RuntimeException("Undefined variable: " + name);
+        }
+        return variables.get(name);
+    }
+    public boolean exists(String name){
+        return variables.containsKey(name);
+    }
+    public String toString(){
+        return variables.toString();
+    }
 }
