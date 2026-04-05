64b95da13e39c6c5533cf926db83a6c5052b86d3
preranarajnag <preranarajnag2809@gmail.com>
2026-04-05 21:15:27 +0530

    Implement main method for code interpretation

--- Main.java
+++ Main.java
@@ -1,1 +1,28 @@
+import Tokenizer.*;
+import Parser.*;
+import Evaluator.*;
 
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String code = """
+                i := 1
+                @ 4 =>
+                >> i
+                i := i + 1
+                """;
+
+        Tokenizer tokenizer = new Tokenizer(code);
+        List<Token> tokens = tokenizer.tokenize();
+
+        Parser parser = new Parser(tokens);
+        List<Instruction> instructions = parser.parse();
+
+        Interpreter interpreter = new Interpreter();
+        interpreter.execute(instructions);
+
+        System.out.println("Final State: " + interpreter.getEnvironment());
+    }
+}
