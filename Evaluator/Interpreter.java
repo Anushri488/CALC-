1a65b0e83defaf2676189207823f16687961032a
preranarajnag <preranarajnag2809@gmail.com>
2026-04-05 21:06:54 +0530

    Implement Interpreter class with execution logic

--- Evaluator/Interpreter.java
+++ Evaluator/Interpreter.java
@@ -1,5 +1,23 @@
 package Evaluator;
 
+import Parser.Instruction;
+import java.util.List;
+
 public class Interpreter {
 
+    private final Environment env;
+
+    public Interpreter() {
+        this.env = new Environment();
+    }
+
+    public void execute(List<Instruction> instructions) {
+        for (Instruction instruction : instructions) {
+            instruction.execute(env);
+        }
+    }
+
+    public Environment getEnvironment() {
+        return env;
+    }
 }
