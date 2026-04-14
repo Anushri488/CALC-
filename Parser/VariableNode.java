4ac038b586e55d7af9e2a5e24d8ca6d2c7d31e6d
Janu-Chaudhary <januchaudhary2004@gmail.com>
2026-04-14 15:52:00 +0530

    docs/test:Documented and implement VariableNode tests.

--- Parser/VariableNode.java
+++ Parser/VariableNode.java
@@ -2,13 +2,30 @@ package Parser;
 
 import Evaluator.Environment;
 
+/**
+ * Represents a variable lookup node in the Abstract Syntax Tree (AST).
+ * When evaluated, it queries the execution environment to retrieve the
+ * current value dynamically associated with its identifier.
+ */
 public class VariableNode implements Expression {
     private final String name;
 
+    /**
+     * Constructs a new VariableNode.
+     *
+     * @param name The identifier/name of the variable (e.g., "x", "counter").
+     */
     public VariableNode(String name) {
         this.name = name;
     }
 
+    /**
+     * Evaluates the variable node by retrieving its value from the environment.
+     *
+     * @param env The execution environment containing currently defined variables.
+     * @return The value assigned to the variable.
+     * @throws RuntimeException if the variable has not been defined in the environment.
+     */
     @Override
     public Object evaluate(Environment env) {
         return env.get(name);
